367562c1bd55098985d52a01c287298f30dda356
Garnica1999 <wcarlosfg.1234567890@hotmail.com>
2018-09-22 22:54:53 -0500

    Cambio
    Eliminado contenido test
    
    Co-Authored-By: garnica1999 <garnica1999@users.noreply.github.com>

--- test/controllers/estudiante/horario_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Estudiante::HorarioControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
-    get estudiante_horario_login_url
-    assert_response :success
-  end
-
-end
--- test/controllers/estudiante_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EstudianteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get estudiante_index_url
-    assert_response :success
-  end
-
-end
